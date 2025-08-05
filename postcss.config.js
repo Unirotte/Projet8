48b6c7dd8b07452e9e61ed308a5a984272f60ead
Unirotte <PichenotJudith@live.fr>
2025-08-05 11:33:38 +0200

    purges de css et push avant essaie de modification de class

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss').default;
 module.exports = {
   plugins: [
     purgecss({
-      content: ['./index.html', './assets/**/*.js'],
+      content: ['./index.html', './assets/scripts.js', './assets/maugallery.js', './assets/bootstrap/bootstrap.bundle.js'],
       safelist: [
         'show', 'collapse', 'active', 'modal', 'fade', 'open', 'dropdown-menu', 
         /^btn/, /^nav/, /^carousel/, /^alert/, /^tooltip/
